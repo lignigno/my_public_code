bfb8cf42fc9ab1bc5f9bef4c34e0d18596ce27be
Liquorice Ignignokt <lignigno@un-c5.kzn.21-school.ru>
2021-11-04 01:16:39 +0300

    added few basic functions ans start writeting play sound

--- plyhi/for_buzzer/srcs/buzzer_controls.c
+++ plyhi/for_buzzer/srcs/buzzer_controls.c
@@ -6,7 +6,7 @@
 /*   By: lignigno <lignign@student.21-school.ru>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/29 07:53:19 by lignigno          #+#    #+#             */
-/*   Updated: 2021/11/02 10:46:53 by lignigno         ###   ########.fr       */
+/*   Updated: 2021/11/04 01:12:40 by lignigno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,42 +15,163 @@
 
 // _____________________________________________________________________________
 
-void  Sart_DMA(uint32_t *pData, uint16_t Length)
-{
-	HAL_TIM_PWM_Start_DMA(&htim1, TIM_CHANNEL_1, (uint32_t *)pData, Length);
-}
+// void  Sart_DMA(uint32_t *pData, uint16_t Length)
+// {
+// 	HAL_TIM_PWM_Start_DMA(&htim1, TIM_CHANNEL_1, (uint32_t *)pData, Length);
+// }
 
-void  Stop_DMA(void)
-{
-  HAL_TIM_PWM_Stop_DMA(&htim1, TIM_CHANNEL_1);
-}
+// void  Stop_DMA(void)
+// {
+//   HAL_TIM_PWM_Stop_DMA(&htim1, TIM_CHANNEL_1);
+// }
+
+// // _____________________________________________________________________________
+
+// void  SetSound(uint64_t hz, uint8_t volume)
+// {
+// 	volume = volume < 101 ? volume : 100;
+
+// 	DMA1_Channel2->CCR |= DMA_CCR_CIRC;
+// 	TIM1->ARR = 79;
+// 	TIM1->PSC = MKHz / (LEN_SINGNAL * TIM1->ARR * hz);
+
+// 	singleSignal[0] = volume;
+// 	Sart_DMA((uint32_t *)singleSignal, LEN_SINGNAL);
+// }
+
+// void  UnSetSound(void)
+// {
+// 	Stop_DMA();
+// }
+
+// void	pilick(uint8_t count, uint8_t volume)
+// {
+// 	for(size_t i = 0; i < count; ++i)
+// 	{
+// 	  SetSound(500, volume);
+// 	  HAL_Delay(300);
+// 	  UnSetSound();
+// 	  HAL_Delay(100);
+// 	}
+// }
 
 // _____________________________________________________________________________
 
-void  SetSound(uint64_t hz, uint8_t volume)
+void	InitSound(hz_t *sound, u64_t arr_size)
 {
-	volume = volume < 101 ? volume : 100;
+	size_t i = sizeof(duration_t);
 
-	DMA1_Channel2->CCR |= DMA_CCR_CIRC;
-	TIM1->ARR = 100;
-	TIM1->PSC = MKHz / (LEN_SINGNAL * TIM1->ARR * hz);
-
-	singleSignal[0] = volume;
-	Sart_DMA((uint32_t *)singleSignal, LEN_SINGNAL);
-}
-
-void  UnSetSound(void)
-{
-	Stop_DMA();
-}
-
-void	pilick(uint8_t count, uint8_t volume)
-{
-	for(size_t i = 0; i < count; ++i)
+	--arr_size;
+	while (i < arr_size)
 	{
-	  SetSound(500, volume);
-	  HAL_Delay(300);
-	  UnSetSound();
-	  HAL_Delay(100);
+		sound[i] = NOT_NOTE;
+		++i;
 	}
+	sound[i] = END_SOUND;
+}
+
+//                                                                             |
+// ----------------------------------------------------------------------------|
+//                                                                             |
+
+void	SetDuration(hz_t *sound, duration_t duration)
+{
+	u8_t	i;
+	u8_t	size;
+	u8_t	offset;
+
+	size = sizeof(duration_t) - 1;
+	i = 0;
+	while (i <= size)
+	{
+		offset = (size - i) * 8;
+		sound[i] = (duration & (0xFF << offset)) >> offset;
+		++i;
+	}
+}
+
+//                                                                             |
+// ----------------------------------------------------------------------------|
+//                                                                             |
+
+void	AddFrequency(hz_t *sound, double hz)
+{
+	size_t	i;
+	double	note;
+	double	base_note[] = {17, 18, 19, 20, 21, 23, 24, 25, 27, 29, 30, 31};
+
+	i = sizeof(duration_t);
+	while (sound[i] < RESERVED)
+		++i;
+	if (sound[i] == END_SOUND)
+		return ;
+	for (size_t octave = 0; octave < 10; ++octave)
+	{
+		note = hz / (1 << octave);
+		if (note < 31.5)
+		{
+			sound[i] = octave * 12;
+			for (u8_t num_note = 0; num_note < 12; ++num_note)
+			{
+				if (note < base_note[num_note])
+				{
+					sound[i] += num_note;
+					sound[i] = sound[i] > 47 ? sound[i] : 48;
+					return ;
+				}
+			}
+		}
+	}
+	sound[i] = 119;
+}
+
+//                                                                             |
+// ----------------------------------------------------------------------------|
+//                                                                             |
+
+void	RemoveFrequency(hz_t *sound, u64_t position)
+{
+	size_t i;
+
+	i = sizeof(duration_t);
+	position += i;
+	while (sound[i] < RESERVED && position != i)
+		++i;
+	if (sound[i] > RESERVED - 1)
+		return ;
+	while (sound[i] < RESERVED)
+	{
+		sound[i] = sound[i + 1];
+		++i;
+	}
+	if (sound[i] == END_SOUND)
+		sound[i - 1] = NOT_NOTE;
+}
+
+//                                                                             |
+// ----------------------------------------------------------------------------|
+//                                                                             |
+
+static void	preparing(const hz_t *sound)
+{
+	double	sfa = SIZE_FREQUENCY_ARR;
+	double	curHz;
+	double	notes[] = {C, C_, D, D_, E, F, F_, G, G_, A, A_, B};
+
+	for (size_t i = sizeof(duration_t); sound[i] != END_SOUND; i++)
+	{
+		curHz = (sound[i] % 12) * (1 << (sound[i] / 12));
+		for (double i = 0; i < sfa; ++i)
+		{
+			double engle += 360 / (MC_Hz / curHz) * i;
+			FrequencyArr[i] = sin
+		}
+	}
+	
+}
+
+void	PlaySound(const hz_t *sound)
+{
+	preparing(sound);
+	printf("\nI play:)\n");
 }
--- plyhi/for_buzzer/srcs/incs/buzzer.h
+++ plyhi/for_buzzer/srcs/incs/buzzer.h
@@ -6,7 +6,7 @@
 /*   By: lignigno <lignign@student.21-school.ru>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/29 07:47:41 by lignigno          #+#    #+#             */
-/*   Updated: 2021/11/02 10:47:06 by lignigno         ###   ########.fr       */
+/*   Updated: 2021/11/04 01:00:04 by lignigno         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,15 +19,42 @@
 
 // ______________________________________________________________________DEFINES
 
-#define MC_Hz 32000000
+#define C	16.3516015625
+#define C_	17.32390625
+#define D	18.35404296875
+#define D_	19.4454296875
+#define E	20.60171875
+#define F	21.8267578125
+#define F_	23.1246484375
+#define G	24.49970703125
+#define G_	25.95654296875
+#define	A	27.5
+#define	A_	29.135234375
+#define B	30.86771484375
+
+#define MC_Hz		32000000
+#define AMPLITUDE	79; //TIMx->ARR
+#define SIZE_FREQUENCY_ARR ((size_t)(MC_Hz / (B * (1 << 9))))
 
 typedef u_int8_t	u8_t;	// unsigned 8-bit type
 typedef u_int16_t	u16_t;	// unsigned 16-bit type
 typedef u_int32_t	u32_t;	// unsigned 32-bit type
 typedef u_int64_t	u64_t;	// unsigned 64-bit type
 
-#define LEN_SINGNAL 1
+typedef u8_t	hz_t;
+typedef u16_t	duration_t;
 
-u16_t	singleSignal[LEN_SINGNAL];
+#define INIT_SOUND_SIZE(size) ((size) + sizeof(duration_t) + 1)
+#define END_SOUND (hz_t)-1
+#define NOT_NOTE (hz_t)-2
+#define RESERVED NOT_NOTE
+
+u8_t	FrequencyArr[SIZE_FREQUENCY_ARR];
+
+void	InitSound(hz_t *sound, u64_t arr_size);
+void	SetDuration(hz_t *sound, duration_t duration);
+void	AddFrequency(hz_t *sound, double hz);
+void	RemoveFrequency(hz_t *sound, u64_t position);
+void	PlaySound(const hz_t *sound);
 
 #endif
