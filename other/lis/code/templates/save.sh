4b20ddbac5e01d00672b6759948d0088ed6aaab8
afomin <silentbob@mail.com>
2025-03-11 22:15:50 +0500

    fix save

--- other/lis/code/templates/save.sh
+++ other/lis/code/templates/save.sh
@@ -3,19 +3,7 @@
 cd /tmp/<user>
 
 git add . > /tmp/null
-
-if [ $? -ne 0 ]; then
-	rm -rf /tmp/null
-	exit 1
-fi
-
 git commit -m "save" > /tmp/null
-
-if [ $? -ne 0 ]; then
-	rm -rf /tmp/null
-	exit 1
-fi
-
 git push > /tmp/null
 
 if [ $? -ne 0 ]; then
