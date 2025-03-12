20185eb96231b7ba523e3b71d876306610d41dd3
lignigno <nik.fomin.2001@list.ru>
2025-03-12 16:11:59 +0500

    LIS has relocated to its own repository

--- other/lis/code/menuconfig.sh
+++
@@ -1,4 +0,0 @@
-#!/bin/zsh
-
-printf "\033[1;38;2;255;0;128mmenuconfig\033[0m\n"
-printf "\033[1;38;2;255;0;128mwill be soon\033[0m\n"
--- other/lis/code/templates/deploy.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/zsh
-
-echo "alias lis=\"/tmp/<user>/lis.sh\"" >> ~/.zshrc
-
-printf "\033[1;38;2;0;255;255m"
-printf "lis deployed\n\n"
-printf "for save run "
-printf "\033[1;38;2;255;0;128m"
-printf "\"lis\"\033[0m\n"
